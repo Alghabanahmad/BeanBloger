fc37271071e39b9a0fccf06d45192f3073ea5829
Alghabanahmad <93946056+Alghabanahmad@users.noreply.github.com>
2021-11-30 13:37:11 +0300

    Add New File

---
+++ multimedia/includes/header.php
@@ -0,0 +1,18 @@
+<!doctype html >
+<html lang="ar"dir="rtl">
+<head>
+    <title>Bean Blogger</title>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id="navbar">
+    <ul>
+            <li><a href="index.php">تاريخ القهوة</a></li>
+            <li><a href="chapter1.php">الزراعة</a></li>
+            <li><a href="chapter2.php">الانواع</a></li>
+            <li><a href="chapter3.php">الحمسات</a></li>
+    </ul>
+    </div>
+
+
